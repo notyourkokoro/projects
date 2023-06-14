66bd4aff7b79d47a305350faf67208f6865ed9a3
Sonel <dmitriew.vsevolod@yandex.ru>
2023-06-14 15:51:53 +0800

    Create valid func

--- project/signup/routes.py
+++ project/signup/routes.py
@@ -1,10 +1,47 @@
 from flask import Blueprint, request, flash, redirect, render_template
 from werkzeug.security import generate_password_hash
+from string import ascii_uppercase, ascii_lowercase
 
 from project.models import database, User
 from flask_login import current_user
 
 signup = Blueprint('signup', __name__)
+
+
+def is_validate_password(password):
+    if len(password) >= 8:
+        flag_upper = False
+        flag_lower = False
+        flag_digit = False
+
+        flag_another = False
+
+        for char in password:
+            if char.isdigit():
+                flag_digit = True
+            elif char in ascii_lowercase:
+                flag_lower = True
+            elif char in ascii_uppercase:
+                flag_upper = True
+            else:
+                flag_another = True
+
+        if flag_another:
+            return False
+        return flag_digit and flag_lower and flag_upper
+
+    return False
+
+
+def is_validate_username(username):
+    if len(username) < 4:
+        return False
+
+    for char in username:
+        if char not in ascii_uppercase and char not in ascii_lowercase:
+            return False
+
+    return True
 
 
 @signup.route('/', methods=['POST', 'GET'])
@@ -15,9 +52,19 @@ def register_new_user():
         return redirect('/profile')
 
     if request.method == 'POST':
-        username = request.form['username']
         email = request.form['email']
-        password = generate_password_hash(request.form['password'])
+
+        if is_validate_username(request.form['username']):
+            username = request.form['username']
+        else:
+            flash('Invalid user name', 'error')
+            return redirect('/signup')
+
+        if is_validate_password(request.form['password']):
+            password = generate_password_hash(request.form['password'])
+        else:
+            flash('The password must consist of eight or more characters and only Latin letters and digits', 'error')
+            return redirect('/signup')
 
         try:
             if User.query.filter_by(email=email).first():
